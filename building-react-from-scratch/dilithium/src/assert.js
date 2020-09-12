68e6686f653c732bbd6c25274ab12e0d7877de7b
luoxupan <luoxupan@xiaomi.com>
2020-09-12 08:54:54 +0800

    del code

--- building-react-from-scratch/dilithium/src/assert.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Lightweight replacement for invariant/node assert
-
-module.exports = function assert(condition) {
-  if (!condition) {
-    throw new Error('assertion failure');
-  }
-};
