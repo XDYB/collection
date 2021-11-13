6f854ec11bc0fe730b448eb8e37e2d547e5f678e
luoxupan <luoxupan@xiaomi.com>
2021-11-13 18:14:43 +0800

    save config

--- ts-code/src/index.ts
+++ ts-code/src/index.ts
@@ -1,24 +1,12 @@
 
-export const add = (a: number, b: number, c?: string | number): number => {
-  console.log(a + b);
-  return a + b;
-};
-add(1, 2);
-
-export const fun = (s: Array<string>, a: number[]) => {
-  console.log(s);
-};
-
-type fun1Type = string | number | boolean;
-export const fun1 = (arg1: fun1Type) => {
-  console.log(arg1);
-};
-fun1(123);
-
-// 枚举
-enum Gender {
-  Male = 0,
-  Female = 1
+function promise() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(1);
+    }, 3000)
+  })
 }
-export const isMale = Gender.Male === 0;
 
+const fun = async () => {
+  
+}
